a183dbc1402571bfe27b97c9d1622c8c0ef95539
shuaib shaban <shuaib>
2022-11-28 09:41:30 +0300

    Set repo

---
+++ index.js
@@ -0,0 +1,36 @@
+// student-generator code
+
+let grade = (prompt("please enter your score: ")); // asks the user to input their score
+
+const Score = 0-100;
+
+
+ if(Score>=79){
+
+    return grade = 'A';
+
+
+}else if(Score>=60 && Score<=79){
+
+    return grade = 'B-';
+
+} else if(Score>=49 && Score<=59){
+
+    return grade = 'C-';
+
+} else if(Score>=40 && Score<=49){
+
+    return grade = 'D-';
+
+}else if(Score<40 && grade>=0){
+  
+    return grade = 'E';
+
+//incase Score>100 || Score is out of (0-100)range
+
+}else if(Score > 100){
+
+   return invalid;
+   
+}
+ 
